bed7942e1247aaf7fcd6b9be6f36945d551879b1
Ryan Mogauro <89362786+ryanmogauro@users.noreply.github.com>
2024-10-26 21:04:47 -0400

    Added authorization and sign up/in pages

--- models.py
+++ models.py
@@ -1,16 +1,30 @@
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     userID  = db.Column(db.Integer, primary_key=True)
-    employeeID = db.Column(db.Integer, db.Foreign_Key('employee'))
+    employeeID = db.Column(db.Integer, db.ForeignKey('employee.employeeID'))
     username = db.Column(db.String(50), unique = True, nullable=False)
-    passwordHash = db.Column(db.String(128), nullable=False)
-    email = db.Column(db.String(50), unique=True)
+    passwordHash = db.Column(db.String(128), nullable= True) #nullable so we can create hash before storing
+    email = db.Column(db.String(50), unique=True, nullable=False)
     isActive = db.Column(db.Boolean, default=True)
     employee = db.relationship('Employee', backref = 'user')
+    
+    def get_id(self):
+        """Override get_id to use userID as the identifier."""
+        return str(self.userID)
+
+    def set_password(self, password):
+        """Hash the password and store it."""
+        self.passwordHash = generate_password_hash(password)
+
+    def check_password(self, password):
+        """Check hashed password."""
+        return check_password_hash(self.passwordHash, password)
 
 
 class ShiftAssignment(db.Model):
@@ -28,7 +42,6 @@ class Employee(db.Model):
     minHours = db.Column(db.Integer, nullable=False)
     maxHours = db.Column(db.Integer, nullable=False)
     gradYear = db.Column(db.Integer)
-    shifts = db.relationship('ShiftAssignment', backref = 'employee')
     unavailabilities = db.relationship('Unavailability', backref = 'employee')
    
 
@@ -36,7 +49,6 @@ class Shift(db.Model):
     shiftID = db.Column(db.Integer, primary_key=True)
     shiftStartTime = db.Column(db.DateTime, nullable=False)
     shiftEndTime = db.Column(db.DateTime, nullable=False)
-    shiftAssignments = db.relationship('ShiftAssignment', backref='shift')
     
     
 
@@ -45,4 +57,3 @@ class Unavailability(db.Model):
     employeeID = db.Column(db.Integer, db.ForeignKey('employee.employeeID'))
     unavailableStartTime = db.Column(db.DateTime)
     unavailableEndTime = db.Column(db.DateTime)
-    employee = db.relationship('Employee', backref='unavailabilities')
---
+++ views.py
@@ -0,0 +1,54 @@
+from flask import Blueprint, render_template, redirect, url_for
+from flask import request
+from models import db, User, Employee, Unavailability, Shift, ShiftAssignment
+from flask_login import login_required, current_user
+from datetime import datetime, timedelta
+
+# Create a blueprint
+main_blueprint = Blueprint('main', __name__)
+
+@main_blueprint.route('/', methods=['GET', 'POST'])
+@login_required
+def schedule():
+    if request.method == 'POST':
+        #logic for admin writing a new schedule
+        pass
+    
+    weekStartDay, weekEndDay = getWeekBounds(datetime.now()) #will need to change datetime.now for viewing different weeks
+    
+    shifts = Shift.query.filter(Shift.shiftStartTime.between(weekStartDay, weekEndDay)).all()
+    
+    return render_template('schedule.html', shifts=shifts) #need to make schedule.html interface
+
+
+
+#maybe make a method for route /myschedule too
+
+    
+def getWeekBounds(date):
+     # Calculate date of Monday date week
+    start_of_week = date - timedelta(days=(date.weekday()))
+    
+    # Calculate date of Sunday of date week
+    end_of_week = start_of_week + timedelta(days=6)
+    
+    return start_of_week, end_of_week
+
+@main_blueprint.route('/unavailability', methods=['GET', 'POST'])
+@login_required
+def unavailability():
+    if request.method == 'POST':
+        #logic to add/update unavailability preferences
+        pass
+    
+    currentUnavailability = Unavailability.filter_by(employeeID = current_user.employeeID).all()
+    
+    return render_template('unavailability.html', unavailability = currentUnavailability)
+
+
+
+
+
+
+
+
