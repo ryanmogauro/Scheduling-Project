4ceab509cfa8e552b135e8b0f2e7faf0e2578b72
Ryan Mogauro <89362786+ryanmogauro@users.noreply.github.com>
2024-11-03 17:21:25 -0500

    small ui change

--- views.py
+++ views.py
@@ -31,7 +31,12 @@ def schedule():
     
     shifts = Shift.query.filter(Shift.shiftStartTime.between(weekStartDay, weekEndDay)).all()
     
-    return render_template('schedule.html', shifts=shifts) #need to make schedule.html interface
+    employee = Employee.query.filter(Employee.employeeID == current_user.employeeID).first()
+    
+    name = employee.firstName if employee else None
+    
+    
+    return render_template('schedule.html', shifts=shifts, name = name) #need to make schedule.html interface
 
 
 
@@ -53,6 +58,7 @@ def unavailability():
         
         #delete old user unavailability
         Unavailability.delete().where(employeeID = current_user.employeeID).all()
+        
         #overwrite all employee avaialability
         for unavailable in unavailability:
             new_unavailable_span = Unavailability(
